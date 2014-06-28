4681ce6a5ffa80306f4cdf9ddaeb41f179f0dd04
Marc <mjcuva@gmail.com>
2014-06-28 17:52:51 -0500

    Uses mock json

--- The Echo Spot/ESEchoFetcher.m
+++ The Echo Spot/ESEchoFetcher.m
@@ -12,16 +12,28 @@
 
 + (NSArray *)loadRecentEchos{
     
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"testechos" ofType:@"json"];
+    NSError *err;
+    NSData *json = [NSData dataWithContentsOfFile:filePath options:NSDataReadingMappedIfSafe error:&err];
+    if(err) {
+        NSLog(@"%@", [err description]);
+    }
+    NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:json options:NSJSONReadingAllowFragments error:&err];
+    if(err){
+        NSLog(@"%@", [err description]);
+    }
+    NSLog(@"%@", [jsonObject[@"echos"] description]);
+    
     NSMutableArray *returnArray = [[NSMutableArray alloc] init];
     
-    for(int i = 0; i < 10; i++){
-        ESEcho *echo = [[ESEcho alloc] init];
-        echo.title = [NSString stringWithFormat:@"Test %i", i];
-        echo.content = [NSString stringWithFormat: @"This is echo number: %i", i];
-        echo.author = [[ESUser alloc] init];
-        
-        [returnArray addObject:echo];
+    for( NSDictionary *echo in jsonObject[@"echos"]){
+        ESEcho *newEcho = [[ESEcho alloc] init];
+        newEcho.title = echo[@"title"];
+        newEcho.content = echo[@"content"];
+        [returnArray addObject:newEcho];
     }
+    
+    NSLog(@"%@", [returnArray description]);
     
     return returnArray;
 }
