fef31a19e260468126227bfa1e7ac5c3846f2d20
Marc <mjcuva@gmail.com>
2014-06-29 14:00:38 -0500

    Keep constants in one place

---
+++ The Echo Spot/constants.h
@@ -0,0 +1,14 @@
+//
+//  constansts.m
+//  The Echo Spot
+//
+//  Created by Marc Cuva on 6/29/14.
+//  Copyright (c) 2014 The Echo Spot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+#define GREEN_COLOR [UIColor colorWithRed:0/255.0f green:231/255.0f blue:161/255.0f alpha:1.0f]
+#define LIGHT_GRAY_COLOR [UIColor colorWithRed:61/255.0f green:62/255.0f blue:63/255.0f alpha:1.0f]
+#define DARK_GRAY_COLOR [UIColor colorWithRed:43/255.0f green:43/255.0f blue:45/255.0f alpha:1.0f]
