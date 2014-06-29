5ada6979062a046628f77d5abf9ca4b5eff08581
Marc <mjcuva@gmail.com>
2014-06-29 14:00:02 -0500

    added new echo view controller

---
+++ The Echo Spot/ESNewEchoVC.h
@@ -0,0 +1,13 @@
+//
+//  ESNewEchoVC.h
+//  The Echo Spot
+//
+//  Created by Marc Cuva on 6/29/14.
+//  Copyright (c) 2014 The Echo Spot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESNewEchoVC : UIViewController
+
+@end
