5dc5c7c7aeea9b24f0e82f0ecdc6ee6da6061c95
Marc <mjcuva@gmail.com>
2014-07-16 19:15:42 -0500

    Added fields required by api

--- The Echo Spot/ESEchoFetcher.m
+++ The Echo Spot/ESEchoFetcher.m
@@ -7,6 +7,7 @@
 //
 
 #import "ESEchoFetcher.h"
+#import "ESCategory.h"
 
 @implementation ESEchoFetcher
 
@@ -20,6 +21,10 @@
         NSString *filePath = [[NSBundle mainBundle] pathForResource:@"testechos" ofType:@"json"];
         json = [NSData dataWithContentsOfFile:filePath options:NSDataReadingMappedIfSafe error:NULL];
     }
+    
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"testechos" ofType:@"json"];
+    json = [NSData dataWithContentsOfFile:filePath options:NSDataReadingMappedIfSafe error:NULL];
+    
     NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:json options:NSJSONReadingAllowFragments error:&err];
     if(err){
         NSLog(@"%@", [err description]);
@@ -30,7 +35,15 @@
     for( NSDictionary *echo in jsonObject[@"echos"]){
         ESEcho *newEcho = [[ESEcho alloc] init];
         newEcho.title = echo[@"title"];
+        newEcho.echoID = [echo[@"id"] intValue];
+        newEcho.imageURL = [NSURL URLWithString:echo[@"imageURL"]];
+        newEcho.votesUp = [echo[@"votes_up"] intValue];
+        newEcho.votesDown = [echo[@"votes_down"] intValue];
+        newEcho.activity = [echo[@"activity"] intValue];
         newEcho.content = echo[@"content"];
+        newEcho.created = [NSDate dateWithTimeIntervalSince1970:[echo[@"created"] intValue]];
+        newEcho.category = [[ESCategory alloc] initWithName:echo[@"category"][@"name"] andCatID:[echo[@"category"][@"id"] intValue]];
+        newEcho.author = [[ESUser alloc] initWithName:echo[@"user"][@"userName"] andID:[echo[@"user"][@"id"] intValue]];
         [returnArray addObject:newEcho];
     }
     
