e5b51e8b900acf93bebbf1ecc4dfc5510590a784
Marc <mjcuva@gmail.com>
2014-07-16 19:14:24 -0500

    Added category class

---
+++ The Echo Spot/ESCategory.m
@@ -0,0 +1,13 @@
+//
+//  ESCategory.m
+//  The Echo Spot
+//
+//  Created by Marc Cuva on 7/13/14.
+//  Copyright (c) 2014 The Echo Spot. All rights reserved.
+//
+
+#import "ESCategory.h"
+
+@implementation ESCategory
+
+@end
