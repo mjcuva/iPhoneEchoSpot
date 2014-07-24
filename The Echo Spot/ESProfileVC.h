ae1546782af06bf975be329b481c149765e68ea8
Marc <mjcuva@gmail.com>
2014-07-23 21:44:45 -0500

    Started profile view

---
+++ The Echo Spot/ESProfileVC.h
@@ -0,0 +1,13 @@
+//
+//  ESProfileVC.h
+//  The Echo Spot
+//
+//  Created by Marc Cuva on 7/23/14.
+//  Copyright (c) 2014 The Echo Spot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESProfileVC : UIViewController
+
+@end
---
+++ The Echo Spot/ESProfileVC.m
@@ -0,0 +1,30 @@
+//
+//  ESProfileVC.m
+//  The Echo Spot
+//
+//  Created by Marc Cuva on 7/23/14.
+//  Copyright (c) 2014 The Echo Spot. All rights reserved.
+//
+
+#import "ESProfileVC.h"
+#import "ThemeManager.h"
+
+@interface ESProfileVC ()
+
+@end
+
+@implementation ESProfileVC
+
+- (void)viewDidLoad{
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName:[UIColor whiteColor]};
+    self.navigationController.navigationBar.translucent = NO;
+    self.navigationController.navigationBar.barTintColor = [[ThemeManager sharedManager] navBarColor];
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+}
+
+- (IBAction)closeview:(UIBarButtonItem *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
