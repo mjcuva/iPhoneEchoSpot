cad816212fa6e133e1db513f954254ae4523cd17
Marc <mjcuva@gmail.com>
2014-07-28 20:59:33 -0500

    Added class for auth

---
+++ The Echo Spot/ESAuthenticator.h
@@ -0,0 +1,13 @@
+//
+//  ESAuthenticator.h
+//  The Echo Spot
+//
+//  Created by Marc Cuva on 7/27/14.
+//  Copyright (c) 2014 The Echo Spot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ESAuthenticator : NSObject
+
+@end
---
+++ The Echo Spot/ESAuthenticator.m
@@ -0,0 +1,13 @@
+//
+//  ESAuthenticator.m
+//  The Echo Spot
+//
+//  Created by Marc Cuva on 7/27/14.
+//  Copyright (c) 2014 The Echo Spot. All rights reserved.
+//
+
+#import "ESAuthenticator.h"
+
+@implementation ESAuthenticator
+
+@end
