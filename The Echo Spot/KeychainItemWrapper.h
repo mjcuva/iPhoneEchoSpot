560d1ce89cc54fb58818d239d01f451d04243ac3
Marc <mjcuva@gmail.com>
2014-07-31 22:52:46 -0500

    Add keychain stuff

---
+++ The Echo Spot/KeychainItemWrapper.h
@@ -0,0 +1,13 @@
+//
+//  KeychainItemWrapper.h
+//  The Echo Spot
+//
+//  Created by Marc Cuva on 7/31/14.
+//  Copyright (c) 2014 The Echo Spot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeychainItemWrapper : NSObject
+
+@end
---
+++ The Echo Spot/KeychainItemWrapper.m
@@ -0,0 +1,13 @@
+//
+//  KeychainItemWrapper.m
+//  The Echo Spot
+//
+//  Created by Marc Cuva on 7/31/14.
+//  Copyright (c) 2014 The Echo Spot. All rights reserved.
+//
+
+#import "KeychainItemWrapper.h"
+
+@implementation KeychainItemWrapper
+
+@end
