e483ee1da0e7ad400aa42e48e0a44ca51decc32f
Marc <mjcuva@gmail.com>
2014-08-02 14:55:57 -0500

    Login works :smile:

--- The Echo Spot/ESAuthenticator.m
+++ The Echo Spot/ESAuthenticator.m
@@ -7,7 +7,8 @@
 //
 
 #import "ESAuthenticator.h"
-#import "KeychainItemWrapper.h"
+#import "KeychainItemWrapper.h" 
+#import "constants.h"
 
 @implementation ESAuthenticator
 
@@ -20,12 +21,14 @@
     return sharedAuthenticator;
 }
 
+static int currentUser = 0;
+
 - (BOOL)isLoggedIn{
     KeychainItemWrapper *keychainItem = [[KeychainItemWrapper alloc] initWithIdentifier:@"EchoSpot" accessGroup:nil];
     NSString *userID = [keychainItem objectForKey:(__bridge id)kSecAttrAccount];
-    
+    NSLog(@"%@", userID);
     // Need to check if valid id and access key
-    if([userID isEqualToString:@"1"]){
+    if(![userID isEqualToString:@""]){
         return YES;
     }else{
         return NO;
@@ -33,13 +36,41 @@
 }
 
 - (BOOL)loginWithUsername: (NSString *)username andPassword: (NSString *)password{
-    // Login returning success
-    // actually log in
+
     KeychainItemWrapper *keychainItem = [[KeychainItemWrapper alloc] initWithIdentifier:@"EchoSpot" accessGroup:nil];
     
-    // Set to actual id
-    [keychainItem setObject:@"1" forKey:(__bridge id)kSecAttrAccount];
-    return YES;
+    username = [username stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    password = [password stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    NSString *post = [NSString stringWithFormat:@"email=%@&password=%@", username, password];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%lu",(unsigned long)[postData length]];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:[self loginURL]]];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:postData];
+    NSError *error;
+    NSURLResponse *response;
+    NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if(error){
+        NSLog(@"%@", [error description]);
+    }
+    NSString *str=[[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+    
+    NSCharacterSet* notDigits = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    if ([str rangeOfCharacterFromSet:notDigits].location == NSNotFound){
+        [keychainItem setObject:str forKey:(__bridge id)kSecAttrAccount];
+        currentUser = [str intValue];
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
+- (NSString *)loginURL{
+    return [NSString stringWithFormat:@"%@%@", BASE_URL, LOGIN_URL];
 }
 
 - (BOOL)logout{
